4c45634ff930a7c058dddf33578b3f72bfc04bab
Vu Pham <vutpham@outlook.com>
2017-10-07 14:13:28 -0700

    Add joins table queries

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -48,9 +48,10 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    opts = BelongsToOptions.new(name, options)
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
 
     define_method(name) do
+      opts = self.assoc_options[name]
       foreign = opts.send(:foreign_key)
       primary = opts.send(:primary_key)
 
@@ -71,6 +72,7 @@ module Associatable
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
